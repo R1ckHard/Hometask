f3b25e8cdb5679a5d4272031e65d8d0921a9e04d
R1ckHard <rickhard15@gmail.com>
2018-11-04 19:10:34 +0300

    tracer

---
+++ H_T_8/mousemove/mousemove.js
@@ -0,0 +1,36 @@
+const rows = +prompt('Введите кол-во рядов:');
+const cols = +prompt('Введите кол-во колонок:');
+
+function newtable() {
+    let tbl = '<table><tbody>';
+    for (let i = 0; i < rows; ++i) {
+        tbl += '<tr>';
+        for (let j = 0; j < cols; ++j) {
+            tbl += '<td></td>';
+        }
+        tbl += '</tr>';
+    }
+    tbl += '</table></tbody>';
+    document.write(tbl);
+}
+
+function child(f) {
+    let table = document.querySelector('table').firstChild;
+    let row = table.children;
+    for (let i = 0; i < row.length; ++i) {
+        let cols = row[i].children;
+        for (var j = 0; j < cols.length; ++j) {
+            f(cols[j]);
+        }
+    }
+}
+function hovered() {
+    child(function(el) {
+        el.addEventListener('mouseover', function makeHover() {
+            let color = this.style.backgroundColor = 'Red';
+            let timer = setTimeout(() => this.style.backgroundColor = 'White', 3000);
+        });
+    })
+}
+newtable();
+hovered();
